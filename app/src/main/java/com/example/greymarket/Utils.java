641efb3c23b1486bae2eacd00b04a1c805882a62
kishore <kishoredurai7@gmail.com>
2020-08-03 17:09:31 +0530

    new

--- app/src/main/java/com/example/greymarket/Utils.java
+++
@@ -1,7 +0,0 @@
-package com.example.greymarket;
-
-public class Utils {
-    public  static  final String EMAIL="n.m.vishwajithking10@gmail.com";
-    public static  final  String PASSWORD= "rokdgnuhxzxoxmwa";
-}
-
